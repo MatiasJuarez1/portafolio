e4452f25435de78c2dd01a9e6a7816698833a1f3
Matias <matiasjuarezz062@gmail.com>
2024-05-24 00:00:04 -0300

    segundo commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,52 @@
-// import foto from "../assets/foto-tp1.jpg";
+import foto from "../assets/foto-tp1.jpg";
 
-const Main = () => {
-  const alumno = {
-    nombre: "Matias Juarez",
-  };
+const Main = ({ me }) => {
 
   return (
-    <main>
+    <main className="bg-neutral-950">
       <section className="bg-main">
         <div className="bg-black/70 backdrop-blur-[3px]">
-          <div className="container min-h-screen flex items-center justify-center gap-7">
-            {/* <img src={foto} alt="matias" className="rounded-full w-96 h-96 object-cover bg-center" /> */}
+          <div className="container min-h-screen flex flex-col items-center justify-center gap-7 relative">
             <h1 className="text-5xl text-center font-extralight tracking-wide text-neutral-50 montserrat">
-              Bienvenidos a mi portafolio, soy Matias Juarez, desarrollador Web
+              Bienvenidos a mi portafolio, soy {me.name} {me.surname}, desarrollador Web
             </h1>
+            <p className="kaushan-script italic text-2xl font-medium text-neutral-400 absolute bottom-10 left-1/2 -translate-x-1/2">
+              &quot;{me.frase}&quot;
+            </p>
           </div>
         </div>
       </section>
-      <section className="min-h-screen bg-neutral-950"></section>
+      <section className="container">
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="flex gap-8 mx-48">
+            <img
+              src={foto}
+              alt="matias"
+              className="rounded-xl w-86 h-96 object-cover bg-center"
+            />
+            <div className="text-neutral-50 py-7">
+              <h2 className="text-5xl text-neutral-500 font-semibold mb-3">
+                Sobre mi
+              </h2>
+              <p className="font-extralight text-xl">
+                {me.about}
+              </p>
+
+              <h4 className="text-2xl mt-10 text-neutral-500 font-semibold mb-2">
+                Tecnologías
+              </h4>
+              <p className="kaushan-script italic text-xl font-medium text-neutral-400 mt-3">
+                &quot;{me.frase2}&quot;
+              </p>
+
+              <h2 className="text-2xl text-neutral-500 font-semibold mt-4">
+                Mas sobre mi
+              </h2>
+              <p className="kaushan-script italic text-xl font-medium text-neutral-400 mt-3">Amante del bucle for ❤ </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
---
+++ src/data/index.js
@@ -0,0 +1,7 @@
+export const me = {
+    name: "Matias",
+    surname: "Juarez",
+    about: "Soy un apasionado de la tecnologia, me adentre en el mundo de la programacion en 2023 y actualmente me dedico al freelancing en grupo...",
+    frase: "Discipline, constancy, hard work & TypeScript",
+    frase2: "Yo solo sé que no sé nada",
+}
